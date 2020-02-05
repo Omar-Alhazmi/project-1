f2ebeedd487546442112e900062458f9055d1315
vex-xcc <vex.xcc@gmail.com>
2020-02-05 13:09:07 +0300

    add Rm

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# project-1
+# project-1 Tic Tac Toe
+
+A Tic tac toe game with a Simple design made with HTML CSS and JavaScript track your Win and Draw score.
+
+## Table of Contents
+
+* User Stories
+* Live Project
+
+* Features
+* Built with
+
+## User Stories
+
+*  As a user, I should be able to start a new tic tac toe game .
+*  As a user, I should be able to click on a square to add X first and  then O, and so on .
+*  As a user, I should not be able to click the same square twice .
+*  As a user, I should be shown a message when I win, lose or tie .
+*  As a user, I should not be able to continue playing once I win, lose, or tie .
+*  As a user, I should be able to play the game again wen i click in another round button without refreshing the page and without losing player status .
+*  As a user, I should be able to see the player status player win cunot, draw cunot .
+*  As a user, I should be able to play the game again wen i click in Restart Game  button without refreshing the page to clear  player status . 
+
+
+
+
+
+
+
+
+
+
--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 $(document).ready(function () {
     //declaring a function to creat array and connactet to the table in html 
-    //and  return board  to
-     const gameBoard = function arrayBoard() {
+    const gameBoard = function arrayBoard() {
 
-        let board = ['', '', '',
-            '', '', '',
-            '', '', ''
-        ];
+        let board = [];
 
         $(".cells").each(function (index) {
             board[index] = $(this).text();
@@ -42,7 +38,7 @@ $(document).ready(function () {
         }
     }
     //declaring a winningCase function take board as argument and check the winning Cases
-    const winningCase = function checkWin(board) {
+    const winningCase = function checkWinCases(board) {
 
         return checkWinner(board[0], board[1], board[2]) +
             checkWinner(board[3], board[4], board[5]) +
@@ -56,50 +52,69 @@ $(document).ready(function () {
     //declaring a gameOver function take result as argument and check the result
     //to display wining Message
     const gameOver = function gameOverMessage(result) {
-
+        //  declaring local variable saveing ID boardMessage
+        //  to display message.
         let divMessage = $("#boardMessage");
+        //if result gretor than 0 then 
+        // display message x win 
         if (result > 0) {
             divShow.classList.add('show')
+            //increasing player 1 Wins by 1
             player1Wins++
             player1WinsText.text(player1Wins)
             divMessage.text("X win!");
-            divMessage.css('color', 'white')
+            divMessage.css('color', '#DBDBDB')
+
             return;
+            //else if result leser than 0 then 
+            // display message o win 
         } else if (result < 0) {
             divShow.classList.add('show')
+            //increasing player 2 Wins by 1
             player2Wins++;
             player2WinsText.text(player2Wins)
             divMessage.text("O win!");
+            divMessage.css('color', '#DBDBDB')
             return;
+            //else if result equle to 0 then 
+            // display message Draw. 
         } else {
             divShow.classList.add('show');
             divMessage.text("Match   Draw .");
             drawMatch++;
             drawMatchText.text(drawMatch);
-            divMessage.css('color', 'white')
+            divMessage.css('color', '#DBDBDB')
         }
     };
     //declaring a reset function to reset Game board and players status 
     const reset = function resetGame() {
+        //reset moveCuont to 0
         moveCuont = 0;
+        //reset turn to 1 to set player1 x as defoult
         turn = 1;
+        //Lifting the block from the blocked cells
         $(".cells").unbind('click').one("click", main);
+        //cells emptying
         $(".cells").text('');
+        //remove the div message
         divShow.classList.remove('show')
+        //reset player1Wins to 0
         player1Wins = 0;
         player1WinsText.text(player1Wins);
+        //reset drawMatch to 0
         drawMatch = 0;
         drawMatchText.text(drawMatch);
+        //reset player2Wins to 0
         player2Wins = 0;
         player2WinsText.text(player2Wins);
-        const divMessage = $("#boardMessage");
-        divMessage.css('color', 'gray');
-        divMessage.text('');
     };
     //declaring a rfresh function to reset Game board
     const rfresh = function resetGame() {
+        //reset moveCuont to 0
         moveCuont = 0;
+        //reset turn to 1 to set player1 x as defolt
         turn = 1;
+        //remove the div message
         divShow.classList.remove('show')
         $(".cells").unbind('click').one("click", main);
         $(".cells").text('');
@@ -107,59 +122,85 @@ $(document).ready(function () {
         divMessage.css('color', 'gray');
         divMessage.text('');
     }
-    //declaring a main function to 
+    //declaring a main function 
+    //check the turn and and diploy text for players
     const main = function mainFunction() {
 
-        let board, result;
+
+        let result,board;
         let player1 = '';
         let player2 = '';
 
+        //if turn = 1 then this is player1 turn
         if (turn === 1) {
-
+            //if player 1 does not equal to empty string
+            //and winningCase does not equal to 0
+            //and player 2 does not equal to empty string 
+            //then return 
             if (player1 !== '' || winningCase(gameBoard()) !== 0 ||
                 player2 !== '') {
 
                 return;
             };
- 
+
             player1 = "X"
+            //create event when player1 click 
+            //the cell disply X
             event.target.innerHTML = player1
-        
             board = gameBoard();
             result = winningCase(board);
+            //if result does not equal to 0
+            // then check the gameOver function
             if (result !== 0) {
                 gameOver(result);
                 return;
             };
-
+            //increasing turn by 1
+            //to change the current turn to player 2          
             turn++;
-
             moveCuont++;
 
         } else {
+            //if player 1 does not equal to empty string
+            //and winningCase does not equal to 0
+            //and player 2 does not equal to empty string 
+            //then return 
             if (player1 !== '' || winningCase(gameBoard()) !== 0 ||
                 player2 !== '') {
 
                 return;
             };
+
             player2 = 'O'
+            //create event when player2 click 
+            //the cell disply O
             event.target.innerHTML = player2
             board = gameBoard();
             result = winningCase(board);
+            //if result does not equal to 0
+            // then check the gameOver function
             if (result !== 0) {
                 gameOver(result);
                 return;
             }
+            //decreasing turn by 1
+            //to change the current turn to player 1       
             turn--
             moveCuont++
         };
+        board = gameBoard();
         result = checkWinner(board);
+        //if moveCuont equal to 9
+        // then check the gameOver function
         if (moveCuont === 9) {
             gameOver(result);
             return;
         }
     };
-    $(".cells").one('click', main)
+     //add event listener when one clicking on the cells
+    $(".cells").one('click', main);
+     //add event listener when click the reset button
     $("#reset").click(reset);
+    //add event listener when click the rfresh button
     $("#rfresh").click(rfresh);
 });
